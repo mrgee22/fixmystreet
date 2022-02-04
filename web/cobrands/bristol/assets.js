ae2cf535e9f4ab0de78146a0cbc1e40243d49069
Moray Jones <moray@mysociety.org>
2022-02-04 17:06:28 +0000

    fixup! [Bristol] Adds parks and other layers

--- web/cobrands/bristol/assets.js
+++ web/cobrands/bristol/assets.js
@@ -76,12 +76,6 @@ fixmystreet.assets.add(options, {
 });
 
 fixmystreet.assets.add(options, {
-    asset_category: "Flooding",
-    asset_item: 'flood risk structure',
-    filter_value: 'FRST'
-});
-
-fixmystreet.assets.add(options, {
     asset_category: "Street Light",
     asset_item: 'street light',
     filter_value: [ 'S070', 'S080', 'S100', 'S110', 'S120', 'S170', 'S220', 'S230' ]
